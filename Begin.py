13c648d947a53b8df01ef6d618fbf65a1d42c87b
ChristophGreger <mail@christoph-greger.de>
2024-05-23 22:02:23 +0200

    more fancy code

--- Begin.py
+++ Begin.py
@@ -51,11 +51,52 @@ while True:
     with harvard as source:
         audio = r.record(source)
 
-    try:
-        print(r.recognize_google(audio, language='de-DE'))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand the audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    text = r.recognize_google(audio, language='de-DE')
     print("end")
+
+    from openai import OpenAI
+
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        max_tokens=300,
+        temperature=0,
+        messages=[
+            {"role": "system",
+             "content": "Frag immer nach, wenn du nicht sicher bist, welche Daten in eine Funktion einzufügen sind. Mache nie Annahmen. Stelle sicher, dass Tage vom Nutzer sicher festgelegt werden."},
+            {"role": "user", "content": text}
+        ],
+        tools=[
+            {
+                "type": "function",
+                "function": {
+                    "name": "bekunde_interesse",
+                    "description": "Erlaubt es dem Nutzer, sich in eine Liste einzutragen, um eine Freizeitbeschäftigung zu machen.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "beschaftigung": {
+                                "type": "string",
+                                "description": "Die Beschäftigung, die der Nutzer machen möchte.",
+                            },
+                            "anfangsdatum": {
+                                "type": "string",
+                                "description": "Das Datum, ab dem der Nutzer die Beschäftigung gerne machen würde. Beispiel: '10/11/2022' (DD/MM/YYYY). Wenn nötig berechne das Datum anhand des aktuellen Datums. Heute ist der 21.April 2024"
+                            },
+                            "enddatum": {
+                                "type": "string",
+                                "description": "Das Datum, bis zu dem der Nutzer die Beschäftigung spätestens machen wollen würde. Beispiel: '10/11/2022' (DD/MM/YYYY). Berechne dabei das Datum anhand des aktuellen Datums, wenn nötig."
+                            }
+                        },
+                        "required": ["beschäftigung", "enddatum", "anfangsdatum"],
+                    },
+                },
+            }
+        ]
+    )
+
+    print(completion.choices[0].message)
+
+
     break
